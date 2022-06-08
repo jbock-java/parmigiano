c573a483696d67d105cead63c0ed317487d663a9
jbock <jbock-java@gmx.de>
2022-06-08 23:01:46 +0200

    klein

--- src/test/java/io/parmigiano/KleinFourTest.java
+++ src/test/java/io/parmigiano/KleinFourTest.java
@@ -3,33 +3,37 @@ package io.parmigiano;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
+import static io.parmigiano.Permutation.symmetricGroup;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class KleinFourTest {
 
+    private final List<Permutation> klein = klein();
+
+    @Test
+    void leftCosetEqualRightCoset() {
+        for (Permutation permutation : symmetricGroup(4)) {
+            assertEquals(
+                    Set.copyOf(leftKlein(permutation)),
+                    Set.copyOf(rightKlein(permutation)));
+        }
+    }
+
     @Test
     void testCosetsInS4() {
         Set<Set<Permutation>> cosets = new LinkedHashSet<>();
-        Permutation.symmetricGroup(4)
-                .forEach(g -> {
-                    Set<Permutation> gH = new HashSet<>();
-                    for (Permutation h : klein()) {
-                        gH.add(g.compose(h));
-                    }
-                    cosets.add(gH);
-                });
+        symmetricGroup(4)
+                .stream()
+                .map(this::leftKlein)
+                .map(Set::copyOf)
+                .forEach(cosets::add);
         assertEquals(6, cosets.size());
-        assertTrue(cosets.contains(Set.of(
-                Permutation.create(0, 1).compose(2, 3),
-                Permutation.create(0, 2).compose(1, 3),
-                Permutation.create(0, 3).compose(1, 2),
-                Permutation.identity())));
+        assertTrue(cosets.contains(Set.copyOf(klein)));
         assertTrue(cosets.contains(Set.of(
                 Permutation.create(0, 1),
                 Permutation.create(2, 3),
@@ -57,8 +61,24 @@ class KleinFourTest {
                 Permutation.create(1, 3, 2))));
     }
 
-    private List<Permutation> klein() {
-        List<Permutation> result = new ArrayList<>();
+    private List<Permutation> leftKlein(Permutation g) {
+        List<Permutation> result = new ArrayList<>(klein.size());
+        for (Permutation h : klein) {
+            result.add(g.compose(h));
+        }
+        return result;
+    }
+
+    private List<Permutation> rightKlein(Permutation g) {
+        List<Permutation> result = new ArrayList<>(klein.size());
+        for (Permutation h : klein) {
+            result.add(h.compose(g));
+        }
+        return result;
+    }
+
+    private static List<Permutation> klein() {
+        List<Permutation> result = new ArrayList<>(4);
         result.add(Permutation.create(0, 1).compose(2, 3));
         result.add(Permutation.create(0, 3).compose(1, 2));
         result.add(Permutation.create(0, 2).compose(1, 3));
